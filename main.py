7fcca260db31bc2a02cf8e6fe6480cb07c1a7c10
ssppll89 <ssppll89@gmail.com>
2024-11-17 17:02:51 +0100

    2

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ num_tiles = (img.width // tile_w)
 
 new_img = Image.new("RGBA", ( (img.width // tile_w) * ((tile_s*2)+tile_w)  , (img.height // tile_h) * ((tile_s*2)+tile_h)), (1,1,1,0))
 
-for a in range((num_tiles * num_tiles)+1):
+for a in range((num_tiles * num_tiles)):
 	x = ((img.width // 2) - ((tile_w // 2) * (a//4)) - (tile_w // 2)) + ((tile_w // 2) * (a%num_tiles))
 	y = ((tile_h // 2) * (a % num_tiles))+ ((tile_h // 2) * ( a // num_tiles ))
 	cropped = img.crop((x, y, x+tile_w, y+tile_h))
